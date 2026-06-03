9dab4de26575fdbde449f3d91d51233935acd976
mdujjal67 <mdujjal.mm67@gmail.com>
2026-06-03 18:44:30 +0600

    auth integration using firebase project created

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center mt-10 font-bold'>Welcome to the Home Page</h1>
+        </div>
+    );
+};
+
+export default Home
+;
